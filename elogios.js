66fe93154610598bee5b65474b66cdd319497fb3
lccerqueira <49873075+lccerqueira@users.noreply.github.com>
2022-03-17 01:45:05 -0300

    elogios.js
    mm elogios

---
+++ elogios.js
@@ -0,0 +1,117 @@
+{
+    "anytime" : [
+		"Você me deixa orgulhoso.",
+		"Eu gosto das suas ideias.",
+		"Ser seu pai/mãe/avô/avó me deixa muito feliz.",
+		"Você não precisa ser perfeito para ser uma pessoa fantástica.",
+		"É importante para mim ouvir o que você pensa sobre isso.",
+		"Eu acredito em você.",
+		"Eu acredito em suas habilidades.",
+		"A nossa família não seria a mesma sem você.",
+		"Eu sei que você fez o melhor que podia.",
+		"Eu te aceito por ser quem você é.",
+		"O resultado não foi o que você esperava, mas você fez o certo.",
+		"Você me ajuda muito.",
+		"Você me deixou feliz hoje.",
+		"Eu amo a forma como você é criativo.",
+		"Eu me divirto com você.",
+		"Suas ideias são boas, não tenha medo de falar sobre elas.",
+		"Você pode fazer uma mudança positiva em qualquer coisa que escolher.",
+		"Eu não vejo a hora de te ver.",
+		"Eu gosto de ouvir suas histórias.",
+		"Eu adoro ver o mundo através dos seus olhos.",
+		"Nunca deixe de ser curioso.",
+		"Eu admiro a sua capacidade de tomar suas próprias decisões.",
+		"Essa é uma ótima pergunta.",
+		"Seus amigos têm sorte de conhecer você.",
+		"Essa foi uma escolha inteligente.",
+		"Eu fico feliz em ver você feliz.",
+		"Eu adoro cuidar de você.",
+		"Eu estou aprendendo coisas novas com você todos os dias.",
+		"Você me faz uma pessoa melhor.",
+		"Estou feliz por ter você aqui ao meu lado.",
+		"Eu entendo o seu ponto de vista, mesmo que não concorde com você.",
+		"Eu sinto que você está se tornando mais maduro a cada dia.",
+		"Isso foi muito corajoso da sua parte.",
+		"A maneira como você pediu perdão me fez perceber o quanto maduro você é.",
+		"Eu estou orgulhoso de você por perceber que estava errado.",
+		"Eu sabia que você conseguiria.",
+		"Eu fico tão feliz que você decidiu tentar novamente.",
+		"É sempre importante para mim ouvi-lo.",
+		"A sua existência enche o meu coração de alegria.",
+		"Lembre-se que eu nunca vou deixar de te amar."
+	],
+    "morning" : [
+        "Bom dia Flor do dia!",
+        "O Tavares já trabalhou???",
+        "Hoje é dia de realizações profissionais",
+        "Faça cocô antes de sair de casa!",
+        "Leve alguma coisa pra fazer no trânsito",
+        "Até parece que vocês estão no horario..."
+		"Parabéns! Não ta fazendo mais do que a obrigação!!!"
+    ],
+    "afternoon" : [
+        "Deu tempo de ir ver um negócio???",
+        "Hoje o dia esta beeeem produtivo!",
+        "Pepê e Nenêm são filhas de Glória Maria???"
+		"Cavalo dado, quando pega chuva, acaba a gasolina???"
+		"Num esqueceu de colocar os copos na pia???"
+		"Liga pra sua familia, Desnaturados!!!"
+		"Penteia o cabelo, tá um mafuá danano!"
+		"Tavares falou que você tem Chulé"
+		"Mostrar essa sua beleza até me cansa..."
+    ],
+    "evening" : [
+        "Normalmente você é bom, mas hoje esta de parabéns!",
+        "Se Eu não fosse um espelho eu me assustava com a sua cara...",
+        "Dia corrido né? E a gnt nem fez nada direito...",
+        "Amanhã você tenta dinovo",
+        "Sonha comigo, Alazão!"
+		"Adivinha quem vai dormir mais magra hoje?"
+    ],
+    "snow": [
+        "Snowball battle!"
+    ],
+    "day_sunny" : [
+        "Celta Azul hoje hein???",
+        "Pede pra tocar Rock hoje!",
+        "Dia de Sol, Vai de moto!!!",
+        "Dia lindo de Sol, Vai de roupa confortável",
+        "O dia esta lindo, num vai estragar ele!!!"
+        ],
+    "day_cloudy" : [
+        "O Celta nuviado hoje",
+        "Nublado hoje, mas você tem que trabalhar assim mesmo.",
+        "Tá tão bonito que até o céu se escondeu de você"
+		"Céu nublado, faça exercicios e coma fribras! Hemorroida dói"
+        ],
+    "cloudy" : [
+        ],
+    "cloudy_windy" : [
+    ],
+    "showers" : [
+        ],
+    "rain" : [
+        "São Pedro esta de Ovo virado hoje",
+        "Tá molhado mas você não é de açucar, bora pra vida!",
+        "Chuva é bom pra dormir né? Mas alguem tem que pagar as contas"
+        ],
+    "thunderstorm" : [
+        ],
+    "fog" : [
+    ],
+    "night_clear": [
+        "Noite limpa hoje, pede um Japa"
+    ],
+    "night_cloudy" : [
+    ],
+    "night_showers" : [
+    ],
+    "night_rain" : [
+    ],
+    "night_thunderstorm" : [
+        "It's a stormy night, don't be scared"
+    ],
+    "night_alt_cloudy_windy" : [
+    ]    
+}
